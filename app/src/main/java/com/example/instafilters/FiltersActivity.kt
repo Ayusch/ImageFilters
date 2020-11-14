031cecd9a2664419d6185eff9238cb60f47ddd35
Ayusch Jain <ayusch@Ayuschs-MacBook-Pro.local>
2020-11-14 20:39:13 +0530

    final commit

--- app/src/main/java/com/example/instafilters/FiltersActivity.kt
+++ app/src/main/java/com/example/instafilters/FiltersActivity.kt
@@ -7,17 +7,31 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.zomato.photofilters.FilterPack
 
 class FiltersActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
+    lateinit var rvFilters: RecyclerView
+    var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filters)
         imageView = findViewById(R.id.iv_main)
+        rvFilters = findViewById(R.id.rv_filters)
 
-        val imageUri = intent.getParcelableExtra<Uri>("imageUri")
+
+        imageUri = intent.getParcelableExtra<Uri>("imageUri")
         Glide.with(this).load(imageUri).into(imageView)
+
+        processRecyclerView()
     }
 
+    fun processRecyclerView() {
+        rvFilters.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val adapter = FiltersAdapter(FilterPack.getFilterPack(this), imageUri)
+        rvFilters.adapter = adapter
+    }
 }
--- app/src/main/java/com/example/instafilters/FiltersAdapter.kt
+++ app/src/main/java/com/example/instafilters/FiltersAdapter.kt
@@ -1,19 +1,22 @@
 package com.example.instafilters
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.zomato.photofilters.imageprocessors.Filter
 
-class FiltersAdapter() : RecyclerView.Adapter<FiltersAdapter.FiltersViewHolder>() {
+class FiltersAdapter(val data: List<Filter>, val imageUri: Uri?) : RecyclerView.Adapter<FiltersAdapter.FiltersViewHolder>() {
 
-    var data = ArrayList<ThumbnailItem>()
-
-    constructor(incomingData: ArrayList<ThumbnailItem>): this() {
-        data = incomingData
-    }
+//    var data = ArrayList<ThumbnailItem>()
+//
+//    constructor(incomingData: ArrayList<ThumbnailItem>): this() {
+//        data = incomingData
+//    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FiltersViewHolder {
         val root = LayoutInflater.from(parent.context).inflate(R.layout.filter_item, parent, false)
@@ -25,7 +28,10 @@ class FiltersAdapter() : RecyclerView.Adapter<FiltersAdapter.FiltersViewHolder>(
     }
 
     override fun onBindViewHolder(holder: FiltersViewHolder, position: Int) {
-        holder.textView.text = "Desi Coder"
+        holder.textView.text = data[position].name
+        imageUri?.let {
+            Glide.with(holder.imageView.context).load(it).into(holder.imageView)
+        }
     }
 
 
